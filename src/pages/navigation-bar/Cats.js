45f079746e23f3fff23faeef1434b8f667cd2423
Annoh Karlgusta <annoh.karlgusta@gmail.com>
2022-06-10 09:49:13 +0300

    Feat: Cats page and functionalities

---
+++ src/pages/navigation-bar/Cats.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Cats = () => {
+    return (
+        <div>
+            <h3>Cats</h3>
+            <div>
+                <img src="../../images/cats.jpg" />
+                <img src="../../images/cats.jpg" />
+            </div>
+        </div>
+    );
+}
+
+export default Cats;
