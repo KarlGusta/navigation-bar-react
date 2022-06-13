a0e77a3b5e9e9aee688eea9c75d987f107450652
Annoh Karlgusta <annoh.karlgusta@gmail.com>
2022-06-13 11:47:26 +0300

    Feat: add 2 pages

---
+++ src/pages/navigation-bar/Goats.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Goats = () => {
+    return (
+        <div>
+            <h3>Goats</h3>
+            <div>
+                <img src="../../images/goats.jpg" />
+                <img src="../../images/goats.jpg" />
+            </div>
+        </div>
+    );
+}
+
+export default Goats;
---
+++ src/pages/navigation-bar/Sheep.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Sheep = () => {
+    return (
+        <div>
+            <h3>Sheep</h3>
+            <div>
+                <img src="../../images/sheep.jpg"/>
+                <img src="../../images/sheep.jpg"/>
+            </div>
+        </div>
+    );
+}
+
+export default Sheep;
